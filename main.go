9e77b89873d96a4983f8da4dae8340ef41956fe4
negibokken <negibokken@gmail.com>
2017-05-04 00:55:10 +0900

    WIP: Add browse function

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 )
 
 func main() {
@@ -15,15 +17,15 @@ func main() {
 	region := os.Args[2]
 	fmt.Println(service)
 	fmt.Println(region)
-	// url := fmt.Sprintf("https://console.aws.amazon.com/%s/home?region=%s#/home", service, region)
-	// switch runtime.GOOS {
-	// case "linux":
-	// 	exec.Command("xdg-open", url).Start()
-	// case "windows":
-	// 	exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	// case "darwin":
-	// 	exec.Command("open", url).Start()
-	// default:
-	// 	fmt.Println("Your PC is not supported.")
-	// }
+	url := fmt.Sprintf("https://console.aws.amazon.com/%s/home?region=%s#/home", service, region)
+	switch runtime.GOOS {
+	case "linux":
+		exec.Command("xdg-open", url).Start()
+	case "windows":
+		exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "darwin":
+		exec.Command("open", url).Start()
+	default:
+		fmt.Println("Your PC is not supported.")
+	}
 }
