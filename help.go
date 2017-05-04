b5851d74805bcbaffd8010e8e2a9a2c37843c29d
negibokken <negibokken@gmail.com>
2017-05-04 17:48:44 +0900

    Add help message file

---
+++ help.go
@@ -0,0 +1,19 @@
+package main
+
+const AppHelp = `NAME:
+   {{.Name}} - {{.Usage}}
+
+USAGE:
+   {{.Name}} [options] [arguments...]
+
+VERSION:
+   {{.Version}}{{if or .Author .Email}}
+
+AUTHOR:{{if .Author}}
+  {{.Author}}{{if .Email}} - <{{.Email}}>{{end}}{{else}}
+  {{.Email}}{{end}}{{end}}
+
+OPTIONS:
+   {{range .Flags}}{{.}}
+   {{end}}
+`
