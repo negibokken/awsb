01cd094f5a9448f954fee56baadf1c7a6db0193e
negibokken <negibokken@gmail.com>
2017-05-05 00:22:40 +0900

    Add browse part

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"runtime"
 	"sort"
 
 	"github.com/codegangsta/cli"
@@ -43,29 +45,23 @@ func main() {
 			cli.ShowAppHelp(ctx)
 			os.Exit(0)
 		}
+
 		service := ctx.Args().Get(0)
 		region := ctx.Args().Get(1)
+		url := fmt.Sprintf("https://console.aws.amazon.com/%s/home?region=%s#/home", service, region)
 
-		fmt.Println(service, region)
+		switch runtime.GOOS {
+		case "linux":
+			exec.Command("xdg-open", url).Start()
+		case "windows":
+			exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+		case "darwin":
+			exec.Command("open", url).Start()
+		default:
+			fmt.Println("Your PC is not supported.")
+		}
 	}
 
 	app.Run(os.Args)
 
-	// if len(os.Args) != 3 {
-	// 	fmt.Println("")
-	// 	os.Exit(1)
-	// }
-	// service := os.Args[1]
-	// region := os.Args[2]
-	// url := fmt.Sprintf("https://console.aws.amazon.com/%s/home?region=%s#/home", service, region)
-	// switch runtime.GOOS {
-	// case "linux":
-	// 	exec.Command("xdg-open", url).Start()
-	// case "windows":
-	// 	exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
-	// case "darwin":
-	// 	exec.Command("open", url).Start()
-	// default:
-	// 	fmt.Println("Your PC is not supported.")
-	// }
 }
